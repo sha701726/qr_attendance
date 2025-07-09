3ccdd02b76a231d23934a941b9ecf98407a27f8d
Nitin Kumar <79453963+sha701726@users.noreply.github.com>
2025-07-09 10:12:58 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,14 @@
+# database.py
+import os
+from dotenv import load_dotenv
+from supabase import create_client
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def get_connection():
+    return supabase
