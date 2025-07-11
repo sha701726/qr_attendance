cce21daf3b583d321b93ae0664baf5594dbcc240
Nitin Kumar <79453963+sha701726@users.noreply.github.com>
2025-07-11 17:57:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "https://qr-attendance-sy8f.onrender.com"}})
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
